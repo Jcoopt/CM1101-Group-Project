3d22a49f9fb44b43d7b482f51ecc01ce9baa3bbb
madelaineburton <m@ddie,eu>
2017-10-19 16:25:47 +0100

    Added descriptions to each item

--- items.py
+++ items.py
@@ -1,41 +1,41 @@
 item_keycard = {
 	"id": "keycard",
 
-	"name": "a keycard",
+	"name": "a keycard to the security office",
 
-	"description":
+	"description": "The magic key that can get you into the security office!"
 
 	"mass": 0.4
 }
 
-item_manages_credit_card = {
-	"id": "credit_card",
+item_managers_credit_card = {
+	"id": "managers_credit_card",
 
-	"name": "manage's credit card"
+	"name": "manager's Amex credit card"
 
-	"description":
+	"description": "The manager's shiny golden contactless AND limitless Amex card!"
 
 	"mass": 0.5
 }
 
 item_lunch_coupon = {
 
-    "id": "Item's Id (i.e computer friendly name)",
+    "id": "lunch_coupon",
 
-    "name": "the (human friendly) name of item",
+    "name": "Free meal deal coupon",
     
-    "description":"Item's description",
+    "description":"Coupon for a free meal deal at The Corner Shop (which are so bad they should be free anyway).",
 
     "mass": 0.1
 }
 
 item_combination = {
 
-	 "id": "Item's Id (i.e computer friendly name)",
+	 "id": "combination",
 
-    "name": "the (human friendly) name of item",
+    "name": "the combination to the vault",
     
-    "description":"Item's description",
+    "description":"The only thing that stands between you and the diamonds.",
     
     "mass": 0.1
 }
