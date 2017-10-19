b0fe4d46d7cf1d221ce3a992ce1d1fedca75b60d
CheungSC <tommy84652@gmail.com>
2017-10-19 15:27:21 +0100

    items update

--- items.py
+++ items.py
@@ -12,4 +12,32 @@ item_NAME{
 
 }
 """
+item_keycard = {
+	"id": "keycard",
 
+	"name": "keycard",
+
+	"description":
+	"",
+
+	"mass": 0.5
+}
+
+item_manages_credit_card = {
+	"id": "credit_card",
+
+	"name": "manage's credit card"
+
+	"description":
+	"",
+
+	"mass": 0.5
+}
+
+item_lunch_coupon = {
+	"id": ""
+}
+
+item_combination = {
+	
+}
