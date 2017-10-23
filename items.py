a991bc9baa14310db5c4e922989f7ecc4b9a0352
CheungSC <tommy84652@gmail.com>
2017-10-23 17:53:50 +0100

    Added items that can find in the shop and your home.

--- items.py
+++ items.py
@@ -3,7 +3,7 @@ item_keycard = {
 
 	"name": "a keycard to the security office",
 
-	"description": "The magic key that can get you into the security office!"
+	"description": "The magic key that can get you into the security office!",
 
 	"mass": 0.4
 }
@@ -11,31 +11,122 @@ item_keycard = {
 item_managers_credit_card = {
 	"id": "managers_credit_card",
 
-	"name": "manager's Amex credit card"
+	"name": "manager's Amex credit card",
 
-	"description": "The manager's shiny golden contactless AND limitless Amex card!"
+	"description": "The manager's shiny golden contactless AND limitless Amex card!",
 
 	"mass": 0.5
 }
 
 item_lunch_coupon = {
-
     "id": "lunch_coupon",
 
-    "name": "Free meal deal coupon",
+    "name": "free meal deal coupon",
     
-    "description":"Coupon for a free meal deal at The Corner Shop (which are so bad they should be free anyway).",
+    "description": "Coupon for a free meal deal at The Corner Shop (which are so bad they should be free anyway).",
 
     "mass": 0.1
 }
 
 item_combination = {
-
 	 "id": "combination",
 
     "name": "the combination to the vault",
     
-    "description":"The only thing that stands between you and the diamonds.",
+    "description": "The only thing that stands between you and the diamonds.",
     
     "mass": 0.1
+}
+
+
+# List of items you can buy in the shop
+item_sneakers = {
+	"id": "sneakers",
+
+	"name": "a pair of sneaker",
+
+	"description": " ",
+
+	"mass": 0.1
+}
+
+item_lunch_box = {
+	"id": "lunch_box",
+
+	"name": "a set of lunch",
+
+	"description": " ",
+
+	"mass": 0.1
+}
+
+item_screwdriver = {
+	"id": "screwdriver",
+
+	"name": "a screwdriver",
+
+	"description": " ",
+
+	"mass": 0.1
+}
+
+item_wire_cutter = {
+	"id": "wire_cutter",
+
+	"name": "a wire cutter",
+
+	"description": " ",
+
+	"mass": 0.1
+}
+
+# List of items you can find in your home
+item_ski_mask = {
+	"id": "ski_mask",
+
+	"name": "a ski mask",
+
+	"description": " ",
+
+	"mass": 0.1
+}
+
+item_backpack = {
+	"id": "backpack",
+
+	"name": "a large backpack",
+
+	"description": " ",
+
+	"mass": 0.1
+}
+
+item_laptop = {
+	"id": "laptop",
+
+	"name": "a laptop",
+
+	"description": " ",
+
+	"mass": 0.1
+}
+
+item_vodka = {
+	"id": "vodka",
+
+	"name": "a bottle of vodka",
+
+	"description": " ",
+
+	"mass": 0.1
+}
+
+item_donats = {
+	"id": "donats",
+
+	"name": "some donats",
+
+	"description": " ",
+
+	"mass": 0.1
 }
